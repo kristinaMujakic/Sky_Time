e466ffa735816474aff15790a5c04ea4c31511e1
krissy <kmujakic@gmail.com>
2023-05-14 19:46:19 +0200

    adding forms

--- forms.py
+++ forms.py
@@ -1,3 +1,21 @@
 '''Forms for Sky Time app'''
 
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class SignUpForm(FlaskForm):
+    '''Form for a user to sign up'''
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LogInForm(FlaskForm):
+    '''Form for a user to login'''
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=8)])
